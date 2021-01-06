d627905ea8bcc9616aa9b8307250df23e987e71e
Ariel Franco Ferreira <afrancfe@everis.com>
2021-01-06 20:17:00 -0300

    :fire: Finished Project :fire:

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://open.mapquestapi.com/geocoding/v1/',
+  baseURL: 'https://open.mapquestapi.com/geocoding/v1/',
 });
 
 export default api;
